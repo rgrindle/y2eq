2b296bdd057743dd0f373ac3601f0ccb43ad2c66
Ryan Grindle <ryan.grindle@uvm.edu>
2020-12-03 14:55:27 -0500

    get_all_one_level is now working

--- data_gathering/grammar.py
+++ data_gathering/grammar.py
@@ -3,6 +3,14 @@ from typing import Dict, Set
 import numpy as np
 
 
+def insert_str(string, to_insert, index):
+    return string[:index] + to_insert + string[index+1:]
+
+
+def get_locs_of(string, what):
+    return [i for i, s in enumerate(string) if s == what]
+
+
 class Grammar:
 
     def __init__(self, V: Set[str],
@@ -41,16 +49,39 @@ class Grammar:
                 new_string_list.append(new_s)
             return ''.join(new_string_list)
 
+    # def gen_all_list(self, str_list=['S'], depth=0, max_depth=7):
+    #     new_str_list = []
+    #     return_list = []
+    #     for s in str_list:
+    #         all_next_level = self.get_all_one_level(s, depth+1, max_depth)
+    #         complete_eq, incomplete_eq = self.get_complete(all_next_level)
+    #         new_str_list.extend(incomplete_eq)
+    #         return_list.extend(complete_eq)
+    #     return_list = self.gen_all_list(str_list, depth+1, max_depth)
+    #     return return_list
+
+    def get_all_one_level(self, string, S_count=None, S_locs=None):
+        S_count = string.count('S') if S_count is None else S_count
+        if 'S' not in string or S_count == 0:
+            return [string]
+        else:
+            S_locs = get_locs_of(string, 'S') if S_locs is None else S_locs
+            next_level = []
+            for p in self.P['S']:
+                next_level.extend(self.get_all_one_level(insert_str(string, p, S_locs[-1]), S_count-1, S_locs[:-1]))
+            return next_level
 
 if __name__ == '__main__':
     G = Grammar(V={'S'},
-                S='S', P={'S': ['x', '(S*S)', '(S+S)',
-                                '(S-S)',
-                                'sin(S)']},
+                S='S', P={'S': ['x', '(S*S)']},#, '(S+S)',
+                                # '(S-S)',
+                                # 'sin(S)']},
                 rng=np.random.RandomState(4))
-    string = G.gen_rand_str()
-    print(string)
-    import sympy
-    x = sympy.symbols('x')
-    expr = sympy.sympify(string)
-    print(expr)
+    next_level = G.get_all_one_level('x*(S*S)')
+    print(next_level)
+    # import sympy
+    # x = sympy.symbols('x')
+    # def format_expr_str(expr_str):
+    #     return sympy.sympify(string).expand()
+    # expr = format_expr_str(string)
+    # print(expr)
