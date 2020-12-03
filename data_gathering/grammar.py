6cd285599fdb78a75e68697590fa9764c10115a9
Ryan Grindle <ryan.grindle@uvm.edu>
2020-12-03 15:05:31 -0500

    gen_all_list now works

--- data_gathering/grammar.py
+++ data_gathering/grammar.py
@@ -49,16 +49,27 @@ class Grammar:
                 new_string_list.append(new_s)
             return ''.join(new_string_list)
 
-    # def gen_all_list(self, str_list=['S'], depth=0, max_depth=7):
-    #     new_str_list = []
-    #     return_list = []
-    #     for s in str_list:
-    #         all_next_level = self.get_all_one_level(s, depth+1, max_depth)
-    #         complete_eq, incomplete_eq = self.get_complete(all_next_level)
-    #         new_str_list.extend(incomplete_eq)
-    #         return_list.extend(complete_eq)
-    #     return_list = self.gen_all_list(str_list, depth+1, max_depth)
-    #     return return_list
+    def gen_all_list(self, str_list=['S'], depth=0, max_depth=7):
+        new_str_list = []
+        return_list = []
+        for s in str_list:
+            all_next_level = self.get_all_one_level(s)
+            complete_eq, incomplete_eq = self.get_complete(all_next_level)
+            new_str_list.extend(incomplete_eq)
+            return_list.extend(complete_eq)
+        if depth < max_depth:
+            return_list.extend(self.gen_all_list(new_str_list, depth+1, max_depth))
+        return return_list
+
+    def get_complete(self, str_list):
+        incomplete = []
+        complete = []
+        for s in str_list:
+            if 'S' in s:
+                incomplete.append(s)
+            else:
+                complete.append(s)
+        return complete, incomplete
 
     def get_all_one_level(self, string, S_count=None, S_locs=None):
         S_count = string.count('S') if S_count is None else S_count
@@ -71,14 +82,19 @@ class Grammar:
                 next_level.extend(self.get_all_one_level(insert_str(string, p, S_locs[-1]), S_count-1, S_locs[:-1]))
             return next_level
 
+
 if __name__ == '__main__':
     G = Grammar(V={'S'},
                 S='S', P={'S': ['x', '(S*S)']},#, '(S+S)',
                                 # '(S-S)',
                                 # 'sin(S)']},
                 rng=np.random.RandomState(4))
-    next_level = G.get_all_one_level('x*(S*S)')
-    print(next_level)
+    # next_level = G.get_all_one_level('x*(S*S)')
+    # print(next_level)
+    eq_list = G.gen_all_list(max_depth=2)
+    print('eq_list', eq_list)
+
+    # print(G.get_complete(['x*(x*x)', 'x*((S*S)*x)', 'x*(x*(S*S))', 'x*((S*S)*(S*S))']))
     # import sympy
     # x = sympy.symbols('x')
     # def format_expr_str(expr_str):
