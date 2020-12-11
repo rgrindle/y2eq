06ebfd47b75ceb6c8e17236a687fb9e402462596
Ryan Grindle <ryan.grindle@uvm.edu>
2020-12-11 14:35:30 -0500

    Convolution in architecture/seq2seq_cnn_attention.py
    -still needs residual connections

--- architecture/seq2seq_cnn_attention.py
+++ architecture/seq2seq_cnn_attention.py
@@ -1,42 +1,45 @@
-from custom_layers import GLU
+from custom_layers import GLU, Attention
 
 # import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.layers import Dense, SimpleRNN, Input, dot, Conv1D
+from tensorflow.keras.layers import Dense, SimpleRNN, Input, dot, Conv1D, Conv2D
 
 LATENTDIM = 256
 INPUT_LENGTH = 1
-NUM_OUTPUT_TOKENS = 21
+NUM_OUTPUT_TOKENS = DICTIONARY_LENGTH = 21
 MAX_OUTPUT_LENGTH = 13
+CONSISTENT_SIZE = 10
 
-encoder_inputs = Input(shape=((30, 1)))
-print(encoder_inputs.shape)
-enc_conv_output = Conv1D(2, 3, padding='same')(encoder_inputs)
-# encoder_outputs, encoder_state = SimpleRNN(LATENTDIM, return_state=True, return_sequences=True)(encoder_inputs)
-# print(encoder_outputs.shape, encoder_state.shape)
-# current thinking that I don't need state, but let's wait
-print(enc_conv_output.shape)
-# print(enc_conv_output[:, :, 0].shape)
-glu_output = GLU()(enc_conv_output)
-print(glu_output.shape)
-exit()
+enc_inputs = Input(shape=((30, 1)))
+print(enc_inputs.shape)
+# TODO: positional encoding here
+enc_conv_outputs = Conv1D(2, 3, padding='same')(enc_inputs)
+print(enc_conv_outputs.shape)
+enc_outputs = GLU()(enc_conv_outputs)
+print(enc_outputs.shape)
+enc_outputs = Dense(CONSISTENT_SIZE)(enc_outputs)
+print(enc_outputs.shape)
+
+print()
+print('begin decoder')
 # get attention weights
 # input last output and current encoder hidden state
-decoder_inputs = Input(shape=((MAX_OUTPUT_LENGTH, NUM_OUTPUT_TOKENS)))
-attention_outputs, attention_state = SimpleRNN(LATENTDIM, return_state=True, return_sequences=True)(decoder_inputs)
-print(attention_outputs.shape, attention_state.shape)
+dec_inputs = Input(shape=((MAX_OUTPUT_LENGTH, NUM_OUTPUT_TOKENS)))
+# TODO: positional encoding here
+# TODO: masking. make sure doesn't see future outputs
+atten_conv_outputs = Conv1D(2*NUM_OUTPUT_TOKENS, 3)(dec_inputs)
+print(atten_conv_outputs.shape)
+atten_outputs = GLU()(atten_conv_outputs)
+print(atten_outputs.shape)
+atten_outputs = Dense(CONSISTENT_SIZE)(atten_outputs)
+print(atten_outputs.shape)
 
-score = dot([encoder_outputs, attention_outputs], axes=(2, 2))
-print(score.shape)
-alignment = keras.activations.softmax(score, axis=-1)
-print(alignment.shape)
-context = dot([alignment, encoder_outputs], axes=(1, 1))
+context = Attention()([enc_outputs, atten_outputs])
 print(context.shape)
 
-dec_rec = SimpleRNN(LATENTDIM, return_sequences=True)(context)
-dec_dense = Dense(NUM_OUTPUT_TOKENS, activation='softmax')(dec_rec)
+dec_dense = Dense(NUM_OUTPUT_TOKENS, activation='softmax')(context)
 
-model = keras.Model([encoder_inputs, decoder_inputs],
+model = keras.Model([enc_inputs, dec_inputs],
                     [dec_dense])
 
 
