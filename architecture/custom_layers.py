2a26f75bca15ea63fdc51253b71b2f7049c7861d
Ryan Grindle <ryan.grindle@uvm.edu>
2020-12-11 14:34:57 -0500

    Updated architecture/custom_layers.py with Attention layer

--- architecture/custom_layers.py
+++ architecture/custom_layers.py
@@ -21,7 +21,26 @@ class GLU(keras.layers.Layer):
 
     def call(self, inputs):
         assert len(inputs.shape) == 3, 'GLU ERROR: Expected 3D input.'
-        assert inputs.shape[2] == 2, 'GLU ERROR: Expected inputs.shape[2] == 2'
-        A = inputs[:, :, 0]
-        B = inputs[:, :, 1]
+        half = inputs.shape[2]//2
+        assert inputs.shape[2]/2 == half, 'GLU ERROR: Expected inputs.shape[2] to be divisible by 2.'
+        A = inputs[:, :, :half]
+        B = inputs[:, :, half:]
         return tf.math.multiply(A, tf.math.sigmoid(B))
+
+
+class Attention(keras.layers.Layer):
+    def __init__(self, units=None, input_dim=None):
+        super(Attention, self).__init__()
+
+    def call(self, inputs):
+        """Expects inputs = [enc_outputs, atten_outputs]"""
+        assert len(inputs) == 2
+        assert inputs[0].shape[2] == inputs[1].shape[2]
+        score = keras.layers.dot(inputs, axes=(2, 2))
+        print('score', score.shape)
+        alignment = keras.activations.softmax(score, axis=-1)
+        print('alignment', alignment.shape)
+        context = keras.layers.dot([alignment, inputs[0]], axes=(1, 1))
+        print('Not quite right need to add something')  # TODO
+        print('context', context.shape)
+        return context
