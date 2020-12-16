3846d49e8688964b3be49fe842c224c03e2bf561
Ryan Grindle <ryan.grindle@uvm.edu>
2020-12-16 14:02:29 -0500

    Updated read location in plotting/plot_history.py
    -Need to to this to keep the read location that same after reorganization

--- plotting/plot_history.py
+++ plotting/plot_history.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('../models/seq2seq_cnn_attention_model_history.csv').values
+data = pd.read_csv('../src/srvgd/utils/models/seq2seq_cnn_attention_model_history.csv').values
 plt.plot(data[:, 0], label='train')
 plt.plot(data[:, 1], label='val')
 plt.xlabel('Epoch')
 plt.ylabel('Symbolic loss')
 plt.legend()
-plt.savefig('plot_history_seq2seq_cnn_attention_model2.pdf')
+plt.savefig('plot_history_seq2seq_cnn_attention_model5.pdf')
