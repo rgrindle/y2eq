48a4e2cf8787c1d2949eea125278e7669a93dd43
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-05 12:10:28 -0500

    Removed white space on otherwise blank lines in jupyter/decoder.py

--- jupyter/decoder.py
+++ jupyter/decoder.py
@@ -4,44 +4,44 @@ import torch.nn.functional as F
 
 
 class Decoder(nn.Module):
-    def __init__(self, 
-                 output_dim, 
-                 emb_dim, 
-                 hid_dim, 
-                 n_layers, 
-                 kernel_size, 
-                 dropout, 
-                 trg_pad_idx, 
+    def __init__(self,
+                 output_dim,
+                 emb_dim,
+                 hid_dim,
+                 n_layers,
+                 kernel_size,
+                 dropout,
+                 trg_pad_idx,
                  device,
-                 max_length = 36):
+                 max_length=36):
         super().__init__()
-        
+
         self.kernel_size = kernel_size
         self.trg_pad_idx = trg_pad_idx
         self.device = device
-        
+
         self.scale = torch.sqrt(torch.FloatTensor([0.5])).to(device)
-        
+
         self.tok_embedding = nn.Embedding(output_dim, emb_dim)
         self.pos_embedding = nn.Embedding(max_length, emb_dim)
-        
+
         self.emb2hid = nn.Linear(emb_dim, hid_dim)
         self.hid2emb = nn.Linear(hid_dim, emb_dim)
-        
+
         self.attn_hid2emb = nn.Linear(hid_dim, emb_dim)
         self.attn_emb2hid = nn.Linear(emb_dim, hid_dim)
-        
+
         self.fc_out = nn.Linear(emb_dim, output_dim)
-        
-        self.convs = nn.ModuleList([nn.Conv1d(in_channels = hid_dim, 
-                                              out_channels = 2 * hid_dim, 
-                                              kernel_size = kernel_size)
+
+        self.convs = nn.ModuleList([nn.Conv1d(in_channels=hid_dim,
+                                              out_channels=2*hid_dim,
+                                              kernel_size=kernel_size)
                                     for _ in range(n_layers)])
-        
+
         self.dropout = nn.Dropout(dropout)
-        
+
         self.relu = nn.ReLU()
-      
+
     def calculate_attention(self, embedded, conved, encoder_conved, encoder_combined):
         
         #embedded = [batch size, trg len, emb dim]
@@ -121,50 +121,50 @@ class Decoder(nn.Module):
         
         for i, conv in enumerate(self.convs):
         
-            #apply dropout
+            # apply dropout
             conv_input = self.dropout(conv_input)
         
-            #need to pad so decoder can't "cheat"
-            padding = torch.zeros(batch_size, 
-                                  hid_dim, 
+            # need to pad so decoder can't "cheat"
+            padding = torch.zeros(batch_size,
+                                  hid_dim,
                                   self.kernel_size - 1).fill_(self.trg_pad_idx).to(self.device)
                 
-            padded_conv_input = torch.cat((padding, conv_input), dim = 2)
+            padded_conv_input = torch.cat((padding, conv_input), dim=2)
         
-            #padded_conv_input = [batch size, hid dim, trg len + kernel size - 1]
+            # padded_conv_input = [batch size, hid dim, trg len + kernel size - 1]
         
-            #pass through convolutional layer
+            # pass through convolutional layer
             conved = conv(padded_conv_input)
 
-            #conved = [batch size, 2 * hid dim, trg len]
+            # conved = [batch size, 2 * hid dim, trg len]
             
-            #pass through GLU activation function
+            # pass through GLU activation function
             conved = F.glu(conved, dim = 1)
 
-            #conved = [batch size, hid dim, trg len]
+            # conved = [batch size, hid dim, trg len]
             
-            #calculate attention
+            # calculate attention
             attention, conved = self.calculate_attention(embedded, 
                                                          conved, 
                                                          encoder_conved, 
                                                          encoder_combined)
             
-            #attention = [batch size, trg len, src len]
+            # attention = [batch size, trg len, src len]
             
-            #apply residual connection
+            # apply residual connection
             conved = (conved + conv_input) * self.scale
             
-            #conved = [batch size, hid dim, trg len]
+            # conved = [batch size, hid dim, trg len]
             
-            #set conv_input to conved for next loop iteration
+            # set conv_input to conved for next loop iteration
             conv_input = conved
             
         conved = self.hid2emb(conved.permute(0, 2, 1))
          
-        #conved = [batch size, trg len, emb dim]
+        # conved = [batch size, trg len, emb dim]
             
         output = self.fc_out((conved))
         
-        #output = [batch size, trg len, output dim]
+        # output = [batch size, trg len, output dim]
             
         return output, attention
