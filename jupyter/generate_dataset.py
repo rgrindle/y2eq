a0c6a60e6d1106986182936da0864251b4e764dc
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-12 14:48:25 -0500

    Updated jupyter/generate_dataset.py
    -don't let duplicate equations be created
    -split into functions
    -added comments/docstrings

--- jupyter/generate_dataset.py
+++ jupyter/generate_dataset.py
@@ -1,7 +1,7 @@
 """
 AUTHOR: Ryan Grindle
 
-LAST MODIFIED: Jan 11, 2021
+LAST MODIFIED: Jan 12, 2021
 
 PURPOSE: Generate train dataset of 50000 observations where
          each observation is a symbolic regression problem/answer
@@ -15,9 +15,11 @@ from eqlearner.dataset.processing import tokenization
 
 import torch
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-
 from sympy import sin, log, exp, Symbol
+
+import os
 
 SEED = 1234
 np.random.seed(SEED)
@@ -30,76 +32,148 @@ if torch.cuda.is_available():
 else:
     from tensor_dataset import TensorDatasetCPU as TensorDataset  # noqa: F401
 
-dataset_size = 50
 
-scaler = MinMaxScaler()
-x = Symbol('x', real=True)
-basis_functions = [x, sin, log, exp]
-support = np.arange(0.1, 3.1, 0.1)
-DC = DatasetCreatorRG(basis_functions,
-                      max_linear_terms=1,
-                      max_binomial_terms=1,
-                      max_compositions=1,
-                      max_N_terms=0,
-                      division_on=False,
-                      random_terms=True,
-                      constants_enabled=True,
-                      constant_intervals_ext=[(-3, 1), (1, 3)],
-                      constant_intervals_int=[(1, 3)])
+def get_dataset(dataset_size, save_loc=None,
+                save_name='_test'):
+    """Create and save dataset. File will be
+    saved in directory save_loc with name
+    'dataset'+save_name+'.pt'
 
-dataset_input = []
-dataset_output = []
-fun_list = []
-count = 0
-cond = True
-while count < dataset_size:
-    fun, dictionary, dictionary_cleaned = DC.generate_fun()
-    if fun != 0:
-        Y = DC.evaluate_function(support, fun, X_noise=False)
-        # numeric = np.array([support, Y])
-        if np.abs(np.max(Y)) < 1000:
-            dataset_input.append(Y)
-            tokenized_eq = tokenization.pipeline([dictionary_cleaned])[0]
-            dataset_output.append(torch.Tensor(tokenized_eq))
-            fun_list.append(fun)
-            count += 1
-        else:
-            print('function out of bounds')
-    else:
-        print('all zeros. fun =', fun)
-print(np.array(dataset_input).shape)
-print()
-for fun in fun_list:
-    print(fun)
-# scaler.fit(np.array(dataset_input).T)
-# x_train_n = scaler.transform(np.array(dataset_input).T)
-# x_train_n = torch.Tensor(x_train_n)
-# l = [len(y) for y in dataset_output]
-# q = np.max(l)
-# dataset_output_p = torch.zeros(len(dataset_output), q)
-# for i, y in enumerate(dataset_output):
-#     dataset_output_p[i, :] = torch.cat([y, torch.zeros(q-y.shape[0])])
-# dataset = TensorDataset(x_train_n.T, dataset_output_p.long())
+    The equations with coefficients will also
+    be saved in save_loc
+    with the name 'equations_with_coeff'+save_name+'.csv'
 
-# x_test = []
-# y_test = []
-# cnt = 0
-# cond = True
-# while cond:
-#     string, dictionary = fun_generator.generate_batch(support,1, X_noise=False, Y_noise=0)
-#     if np.all(string[0][1] == 0) == False:
-#         if np.max(string[0][1]) < 1000 and np.min(string[0][1]) > -1000 and tokenization.get_string(tokenization.pipeline(dictionary)[0])[-1] != '+': 
-#             x_test.append(string[0][1])
-#             y_test.append(torch.Tensor((tokenization.pipeline(dictionary)[0])))
-#             cnt += 1
-#     if cnt == test_eqs:
-#         cond = False
-# scaler = MinMaxScaler()
-# x_test_n = scaler.fit_transform(np.array(x_test).T)
-# x_test_n = torch.Tensor(x_test_n)
-# l = [len(y) for y in y_test]
-# q = np.max(l)
-# y_test_p = torch.zeros(len(y_test), q)
-# for i, y in enumerate(y_test):
-#     y_test_p[i, :] = torch.cat([y, torch.zeros(q-y.shape[0])])
-# test_data = TensorDataset(x_test_n.T, y_test_p.long())
+    And, the y-values will also be saved in save_loc
+    as 'unscaled_yvalues'+save_name+'.csv'
+    """
+    inputs, outputs, eq_list = get_dataset_data(dataset_size)
+    inputs_scaled = scale(inputs)
+    outputs_padded = pad(outputs)
+
+    dataset = TensorDataset(inputs_scaled, outputs_padded.long())
+
+    save(dataset, inputs, eq_list, save_loc, save_name)
+    return dataset
+
+
+def get_dataset_data(dataset_size):
+    """Generate unique equations and
+    record some details. Equations that
+    have y-values outsize of [-1000, 1000]
+    are discarded.
+
+    Parameters
+    ----------
+    dataset_size : int
+        The number of unique equations to create.
+        This is the same as the number of observations
+        in the final dataset.
+
+    Returns
+    -------
+    dataset_input : list
+        The y-values of each unique equation.
+        dataset_input.shape = (dataset_size, 30)
+    dataset_output : list (jagged)
+        The tokenized equation without coefficients.
+        len(dataset_output) = dataset_size. But equations
+        are likely not the same size.
+    eq_list : list
+        A list of equations with coefficients.
+        len(eq_list) = dataset_size. But equations
+        are likely not the same size.
+    """
+    dataset_input = []
+    dataset_output = []
+    eq_list = []
+    count = 0
+    while count < dataset_size:
+        eq, dictionary, dictionary_cleaned = DC.generate_fun()
+        Y = DC.evaluate_function(support, eq, X_noise=False).tolist()
+        if not np.any(np.isnan(Y)):
+            if np.all(np.abs(Y) <= 1000):
+                if Y not in dataset_input:
+                    dataset_input.append(Y)
+                    tokenized_eq = tokenization.pipeline([dictionary_cleaned])[0]
+                    dataset_output.append(torch.Tensor(tokenized_eq))
+                    eq_list.append(eq)
+                    count += 1
+
+    return dataset_input, dataset_output, eq_list
+
+
+def scale(unscaled):
+    """scale unscaled into [0, 1] individually
+    for each equation
+
+    Parameters
+    ----------
+    unscaled : list
+        Data to be scaled. Each row is scaled individually.
+
+    Returns
+    -------
+    scaled : torch.Tensor
+        Scaled data. (unscaled.shape == scaled.shape)
+    """
+    scaled = MinMaxScaler().fit_transform(np.array(unscaled).T)
+    return torch.Tensor(scaled).T
+
+
+def pad(unpadded):
+    """pad unpadded (tokenized equations) so they are
+    all the same length.
+
+    Parameters
+    ----------
+    unpadded : 2D list
+        The data to be padded. Expected to be
+        tokenized equations.
+
+    Returns
+    -------
+    padded : 2D list
+        The padded data. (len(unpadded) = len(padded))
+    """
+    max_len = np.max([len(y) for y in unpadded])
+    padded = [np.hstack((eq_seq, np.zeros(max_len-len(eq_seq)))) for eq_seq in unpadded]
+    return torch.Tensor(padded)
+
+
+def save(dataset, unscaled_y_values, eq_list,
+         save_loc, save_name):
+    if save_loc is None:
+        return
+
+    os.makedirs(save_loc, exist_ok=True)
+
+    # save unscaled y-values
+    filename = os.path.join(save_loc, 'unscaled_yvalues'+save_name+'.csv')
+    pd.DataFrame(unscaled_y_values).to_csv(filename, index=False, header=None)
+
+    # save equations with coefficients
+    filename = os.path.join(save_loc, 'equations_with_coeff'+save_name+'.csv')
+    pd.DataFrame(eq_list).to_csv(filename, index=False, header=None)
+
+    # save dataset
+    filename = os.path.join(save_loc, 'dataset'+save_name+'.pt')
+    torch.save(dataset, filename)
+
+
+if __name__ == '__main__':
+
+    x = Symbol('x', real=True)
+    basis_functions = [x, sin, log, exp]
+    support = np.arange(0.1, 3.1, 0.1)
+    DC = DatasetCreatorRG(basis_functions,
+                          max_linear_terms=1,
+                          max_binomial_terms=1,
+                          max_compositions=1,
+                          max_N_terms=0,
+                          division_on=False,
+                          random_terms=True,
+                          constants_enabled=True,
+                          constant_intervals_ext=[(-3, 1), (1, 3)],
+                          constant_intervals_int=[(1, 3)])
+
+    dataset = get_dataset(dataset_size=5, save_loc='dataset_test')
