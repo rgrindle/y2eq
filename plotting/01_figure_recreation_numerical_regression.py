75aea88529d7f19d88b98fd7c32bb045514a2180
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-14 11:30:44 -0500

    Added plotting/figure_recreation_numerical_regression.py scripts to recreate figure from paper

---
+++ plotting/01_figure_recreation_numerical_regression.py
@@ -0,0 +1,31 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jan 14, 2021
+
+PURPOSE: Take data from last script and plot
+         a CDF to see if it matches paper.
+
+NOTES:
+
+TODO:
+"""
+from cdf import plot_cdf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import os
+
+path = os.path.join('..', 'src', 'srvgd', 'numerical_regression', 'rmse')
+df = pd.read_csv(os.path.join(path, 'all_data.csv'))
+
+for key in df:
+    plt.close('all')
+    plt.figure()
+    data = [v for v in df[key] if 0 <= v <= 10]
+    plot_cdf(data, color='#CF6875')
+    plt.ylabel('Cumulative Counts')
+    plt.xlabel('RMSE on {}'.format(key))
+    plt.xlim([0, 3])
+    plt.ylim([0, 1000])
+    plt.savefig('figure_recreation_numerical_regression_{}.pdf'.format(key))
---
+++ plotting/cdf.py
@@ -0,0 +1,68 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jan 14, 2021
+
+PURPOSE: Provide functions to plot cumulative distribution.
+
+NOTES:
+
+TODO:
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_cdf(X):
+    """Get the probability that x_i > X after X has
+    been sorted (that is x_i is >= i+1 other x's).
+    Parameters
+    ----------
+    X : list
+        A sample from the distribution for
+        which to compute the CDF
+    Returns
+    -------
+    p : list
+        A list of the same length as X that
+        give the probability that x_i > X
+        where X is a randomly selected value
+        and i is the index.
+    """
+
+    X_sorted = sorted(X)
+    n = len(X)
+    p = [i/n for i, x in enumerate(X)]
+    return p, X_sorted
+
+
+def plot_cdf(X, labels=True, label=None, color=None):
+    """Use get_cdf to plot the CDF.
+    Parameters
+    ----------
+    X : list
+        A sample from the distribution for
+        which to compute the CDF
+    labels : bool (default=True)
+        If true, label x-axis x and y-axis Pr(X < x)
+    label : str (default=None)
+        The legend label.
+    color : str (default=None)
+        Color to used in plot. If none, it will not
+        be pasted to plt.step.
+    """
+
+    p, X = get_cdf(X)
+
+    if color is None:
+        plt.fill_between(X, len(X)*np.array(p),
+                         step='post', label=label)
+    else:
+        plt.fill_between(X, len(X)*np.array(p),
+                         step='post', label=label,
+                         color=color)
+
+    if labels:
+        plt.ylabel('$Pr(X < x)$')
+        plt.xlabel('$x$')
