c7332a8f9960ab93e0dc4e5ca6a143a58ff0ad5a
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-15 12:12:34 -0500

    Updated plotting/figure_recreation.py
    -save with file_endname like many of the other scripts do

--- plotting/figure_recreation.py
+++ plotting/figure_recreation.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
     import pandas as pd
     import numpy as np
 
-    rmse_data = pd.read_csv('rmse_test.csv').values[:, 1]
+    file_endname = '_layers10_clip1_dropoutFalse_lr1e-4_2000'
+    rmse_data = pd.read_csv('../jupyter/rmse{}.csv'.format(file_endname)).values[:, 1]
     mask = ~(np.logical_or(np.isnan(rmse_data), np.isinf(rmse_data)))
     print(rmse_data[mask])
     plot_cdf(rmse_data[mask], labels=False, color='#8B94FC')
@@ -14,4 +15,4 @@ if __name__ == '__main__':
     plt.ylabel('Cumulative Counts')
     # plt.xlim([0, 3])
     # plt.ylim([0, 1000])
-    plt.savefig('figure_recreation2.pdf')
+    plt.savefig('figure_recreation{}.pdf'.format(file_endname))
