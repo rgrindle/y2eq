2bf64dbfb9e2ac7f19b23f0f637b6e2accc0c6c4
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-22 13:05:46 -0500

    Added jupyter/tokenization_rg.py
    -This is based on eqlearner.dataset.processing.tokenziation
    -Instead of taking a dictionary, this new version takes a string and breaks it into tokens which it then converts to integers.

---
+++ jupyter/tokenization_rg.py
@@ -0,0 +1,28 @@
+import tokenize
+from io import BytesIO
+
+
+def default_map():
+    default_map = {'x': 1, 'sin': 2, 'exp': 3, 'log': 4, '(': 5, ')': 6, '**': 7, '*': 8, '+': 9,
+                   '/': 10, 'E': 11, 'START': 12, 'END': 13, 'sqrt': 14, '-': 15}
+    max_val = max(list(default_map.values()))
+    numbers = {str(n): max_val+n for n in range(1, 10)}
+    default_map = {**default_map, **numbers}
+    return default_map
+
+
+def numberize_tokens(tokens):
+    mapping = default_map()
+    return [mapping[di] for di in tokens]
+
+
+def extract_tokens(string):
+    string = string.replace('START', '').replace('END', '')
+    extracted_tokens = ['START']+[elem.string for elem in tokenize.tokenize(BytesIO(string.encode('utf-8')).readline)]+['END']
+    extracted_tokens.remove('utf-8')
+    return [e for e in extracted_tokens if e != '']
+
+
+def tokenize_eq(eq_str):
+    extracted_tokens = extract_tokens(eq_str)
+    return numberize_tokens(extracted_tokens)
