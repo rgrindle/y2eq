1f039674ada3fa9a5429ffe30c756c9aa83838a6
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-22 13:07:56 -0500

    Added jupyter/modifying_functional_forms_list.py
    -Identify and fix functional forms

--- jupyter/get_functional_forms.py
+++ jupyter/get_functional_forms.py
@@ -42,9 +42,71 @@ if __name__ == '__main__':
                 ff_counts[i] += 1
     print(ff_counts)
     assert np.all(ff_counts > 0)
-    # import matplotlib.pyplot as plt
-    # plt.plot(sorted(ff_counts))
+
     indices = np.argsort(ff_counts)
 
     for i in indices[::-1]:
         print(ff_counts[i], unique_func_forms[i])
+    exit()
+
+    import matplotlib.pyplot as plt
+
+    def get_cdf(X):
+        """Get the probability that x_i > X after X has
+        been sorted (that is x_i is >= i+1 other x's).
+        Parameters
+        ----------
+        X : list
+            A sample from the distribution for
+            which to compute the CDF
+        Returns
+        -------
+        p : list
+            A list of the same length as X that
+            give the probability that x_i > X
+            where X is a randomly selected value
+            and i is the index.
+        """
+
+        X_sorted = sorted(X)
+        n = len(X)
+        p = [i/n for i, x in enumerate(X)]
+        return p, X_sorted
+
+
+    def plot_cdf(X, labels=True, **kwargs):
+        """Use get_cdf to plot the CDF.
+        Parameters
+        ----------
+        X : list
+            A sample from the distribution for
+            which to compute the CDF
+        labels : bool (default=True)
+            If true, label x-axis x and y-axis Pr(X < x)
+        label : str (default=None)
+            The legend label.
+        color : str (default=None)
+            Color to used in plot. If none, it will not
+            be pasted to plt.step.
+        """
+
+        p, X = get_cdf(X)
+
+        plt.fill_between(X, len(X)*np.array(p),
+                         **kwargs)
+
+        if labels:
+            plt.ylabel('$Pr(X < x)$')
+            plt.xlabel('$x$')
+
+
+    plt.plot(sorted(ff_counts))
+    plt.ylabel('# of instances of\nfunctional form in dataset')
+    plt.xlabel('Each integer corresponds to a functional form\nsorted by y-axis')
+    plt.tight_layout()
+    plt.show()
+    plt.figure()
+    plt.cdf(ff_counts)
+    plt.xlabel('# of instances of\nfunctional forms in dataset')
+    plt.ylabel('Cummulative')
+    plt.show()
---
+++ jupyter/modifying_functional_forms_list.py
@@ -0,0 +1,134 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jan 19, 2021
+
+PURPOSE: Elimiate confusing functional forms. This include
+         functional forms of the form exp(x), exp(2x), exp(3x)
+         which are all really the same.
+
+NOTES: ff = functional form
+
+TODO:
+"""
+import pandas as pd
+
+
+def has_coeffs(ff):
+    """Find coefficients that have slipped into the functional
+    from. I expect these to be found as exp(#*x), but there
+    may be other forms. Note that we need to return false
+    for exponents. Example: x**3
+
+    Parameters
+    ----------
+    ff : str
+        The functional from to check.
+
+    Returns
+    -------
+     : bool
+        Returns true if there are coeffs in ff. Otherwise,
+        returns false.
+    """
+    for i, char in enumerate(ff):
+        if char.isdigit():
+            if i != len(ff)-1:
+                if ff[i+1:i+3] == '*x':
+                    return True
+    return False
+
+
+def fix_ff(ff):
+    """Fix ff if it has instances of 3*x, 2*x, ...
+    Do this by using the lowest numbers possible.
+
+    Parameters
+    ----------
+    ff : str
+        The functional form
+
+    Returns
+    -------
+    ff_fixed : str
+        The fixed functional form
+
+    Example
+    -------
+    >>> fix_ff('exp(3*x)+exp(2*x)')
+    'exp(2*x)+exp(x)'
+    """
+    problem_indices = []
+    for i, char in enumerate(ff):
+        if char.isdigit() and ff[i+1:i+3] == '*x':
+            problem_indices.append(i)
+
+    if 'exp' not in ff == 1:
+        return ff.replace(ff[problem_indices[0]]+'*', '')
+    else:
+        corrected = {i: None for i in problem_indices}
+        for i in reversed(problem_indices):
+            if i+11 >= len(ff):
+                corrected[i] = 'x'
+            elif ff[i+4:i+9] == '+exp(':
+                if i+9 in corrected and corrected[i+9] is not None:
+                    char_of_interest = corrected[i+9]
+                else:
+                    char_of_interest = ff[i+9]
+
+                if char_of_interest == 'x':
+                    corrected[i] = '2'
+                elif char_of_interest.isdigit():
+                    corrected[i] = str(int(char_of_interest)+1)
+                else:
+                    print('This is a situation not concidered', ff)
+                    exit()
+            else:
+                corrected[i] = 'x'
+
+        ff_fixed_list = []
+        prev = 0
+        for i in corrected:
+            ff_fixed_list.append(ff[prev:i])
+            ff_fixed_list.append(corrected[i])
+            prev = i + 1
+            if corrected[i] == 'x':
+                prev += 2
+        ff_fixed_list.append(ff[prev:])
+        return ''.join(ff_fixed_list)
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+    ff_list = pd.read_csv('functional_forms00.csv', header=None).iloc[:, 1].values
+
+    # Find functional forms with 3*x, 2*x, (are there other ints?)
+    # I expect these to only occur for exp. Is that accurate?
+    has_coeffs_list = []
+    no_coeffs_list = []
+    for ff in ff_list:
+        if has_coeffs(ff):
+            has_coeffs_list.append(ff)
+        else:
+            no_coeffs_list.append(ff)
+
+    print(len(has_coeffs_list), len(no_coeffs_list))
+    corrected_list = []
+    for ff in sorted(has_coeffs_list):
+        corrected_list.append(fix_ff(ff))
+
+    fixed_ff_list = no_coeffs_list + corrected_list
+    print(len(np.unique(corrected_list)))
+    unique_ff_list = np.unique(fixed_ff_list)
+    print(len(fixed_ff_list), len(unique_ff_list))
+
+    for i, u_ff in enumerate(unique_ff_list):
+        if 'E*' in u_ff:
+            print(unique_ff_list[i])
+            unique_ff_list[i] = u_ff.replace('E*', '')
+            print(unique_ff_list[i])
+
+    # unique_ff_list = np.unique(unique_ff_list)
+    pd.DataFrame(unique_ff_list).to_csv('unique_ff_list.csv',
+                                        index=False, header=None)    
