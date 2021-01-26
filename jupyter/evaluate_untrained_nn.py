fe9b2b836f465db7aa5794e4720f534e2774d484
Ryan Grindle <ryan.grindle@uvm.edu>
2021-01-26 15:44:54 -0500

    Added jupyter/evaluate_untrained_nn.py
    -Show output of an untrained model

---
+++ jupyter/evaluate_untrained_nn.py
@@ -0,0 +1,34 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jan 26, 2021
+
+PURPOSE: Understand how untrained models behave.
+
+NOTES:
+
+TODO:
+"""
+from analyze_model_00 import translate_sentence
+from get_model import get_model
+
+import torch
+import pandas as pd
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = get_model(device, layers=10)
+test_data = torch.load('dataset_test_ff.pt', map_location=device)
+
+predicted_data = []
+for i, obs in enumerate(test_data):
+
+    inputs, targets = obs
+    predicted = translate_sentence(sentence=inputs,
+                                   model=model,
+                                   device=device,
+                                   max_len=67)[0]
+    predicted_data.append(predicted)
+    # print('.', flush=True, end='')
+    print(i, predicted)
+
+pd.DataFrame(predicted_data).to_csv('untrained_output.csv', index=False)
