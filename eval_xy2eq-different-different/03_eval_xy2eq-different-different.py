f9bd76ae178da394b2860d2f3ceed34c1aaad61b
Ryan Grindle <ryan.grindle@uvm.edu>
2021-02-12 10:25:06 -0500

    Renamed scripts in  eval_xy2eq-different-different/

--- eval_xy2eq-different-different/03_eval_xy2eq-different-different.py
+++ eval_xy2eq-different-different/03_eval_xy2eq-different-different.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     plt.legend()
     plt.xscale('log')
     plt.ylim([0, 1])
-    plt.savefig('03_figure_fixed_x_is_problem_ymax1.pdf')
+    plt.savefig('03_eval_xy2eq-different-different_ymax1.pdf')
 
     plt.figure()
     plot_cdf(rmse_data_fixed, labels=False, color='#8B94FC', label='$x = \\left[0.1, 0.2, \\cdots, 3.0\\right]$ (epochs=900,input=y)')
@@ -64,19 +64,11 @@ if __name__ == '__main__':
     plt.ylim([0, 1000])
     plt.legend()
     plt.xscale('log')
-    plt.savefig('03_figure_fixed_x_is_problem_ymaxauto.pdf')
+    plt.savefig('03_eval_xy2eq-different-different_ymaxauto.pdf')
 
     # make figure x in [0, 3]
-    plt.figure()
-    plot_cdf(rmse_data_fixed, labels=False, color='#8B94FC', label='$x = \\left[0.1, 0.2, \\cdots, 3.0\\right]$ (epochs=900,input=y)')
-    plot_cdf(rmse_data_random_y, labels=False, color='#F49F1C', label='$x \\sim $uniform$\\left([0.1, 3.1)\\right)$ (epochs=900,input=y)')
-    plot_cdf(rmse_data_random_xy_with_1000x, labels=False, color='C9', label='$x \\sim $uniform$\\left([0.1, 3.1)\\right)$ (epochs=1000,input=x,y)')
-    # plot_cdf(rmse_data_random_xy450, labels=False, color='C5', label='$x \\sim $uniform$\\left([0.1, 3.1)\\right)$ (epochs=450,input=x,y)')
-    plot_cdf(rmse_data_gridify, labels=False, color='C7', label='$x \\sim $uniform$\\left([0.1, 3.1)\\right)$ (epochs=900,gridify)')
-
-    plt.xlabel('RMSE on extrapolation region ($x = \\left[3.1, 3.2, \\cdots, 6.0\\right]$)')
-    plt.ylabel('Cumulative counts')
     plt.xlim([0, 3])
     plt.ylim([0, 1000])
+    plt.scale('linear')
     plt.legend()
-    plt.savefig('03_figure_fixed_x_is_problem.pdf')
+    plt.savefig('03_eval_xy2eq-different-different.pdf')
