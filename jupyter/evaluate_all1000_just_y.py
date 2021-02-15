c45b85264c5ba52b9ae550bc141e80048624444c
Ryan Grindle <ryan.grindle@uvm.edu>
2021-02-15 17:00:50 -0500

    Added new evalute function for use on validation dataset during training

---
+++ jupyter/evaluate_all1000_just_y.py
@@ -0,0 +1,31 @@
+import torch
+
+
+def evaluate(model, iterator, criterion, with_x=False):
+    model.eval()
+    epoch_loss = 0
+
+    with torch.no_grad():
+        for i, batch in enumerate(iterator):
+            src = batch[0]
+            trg = batch[1]
+
+            if with_x:
+                src = src[:, :, 1:]
+
+            output, _ = model(src, trg[:, :-1])
+
+            # trg = [trg len, batch size]
+            # output = [trg len, batch size, output dim]
+
+            output_dim = output.shape[-1]
+            output = output.contiguous().view(-1, output_dim)
+            trg = trg[:, 1:].contiguous().view(-1)
+
+            # trg = [(trg len - 1) * batch size]
+            # output = [(trg len - 1) * batch size, output dim]
+
+            loss = criterion(output, trg)
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
