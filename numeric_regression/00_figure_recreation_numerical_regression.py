a5405df5b2d0ef21fe25c7365cbfa4111b2611cb
Ryan Grindle <ryan.grindle@uvm.edu>
2021-03-01 20:52:22 -0500

    Update to numeric_regression/00_figure_recreation_numerical_regression.py
    -Changed save location

--- numeric_regression/00_figure_recreation_numerical_regression.py
+++ numeric_regression/00_figure_recreation_numerical_regression.py
@@ -17,15 +17,16 @@ import pandas as pd
 
 import os
 
+dataset = '_ff1000_with_x'
 all_data = {'train_rmse': [],
             'train_unscaled_rmse': [],
             'test_rmse': [],
             'test_unscaled_rmse': []}
 
 for i in range(1000):
-    data = pd.read_csv(os.path.join('rmse', '{}.txt'.format(i)))
+    data = pd.read_csv(os.path.join('rmse', dataset, '{}.txt'.format(i)))
     for key in all_data:
         all_data[key].append(data[key].values[0])
 
-save_file = os.path.join('rmse', 'all_data.csv')
+save_file = os.path.join('rmse', dataset, 'all_data.csv')
 pd.DataFrame(all_data).to_csv(save_file, index=False)
