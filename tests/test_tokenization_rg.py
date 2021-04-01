071af29c4aaf7127aa647367007d1bcb9e384e4f
Ryan Grindle <ryan.grindle@uvm.edu>
2021-04-01 14:13:32 -0400

    Added tests/test_tokenization_rg.py
    -unit tests to my tokenization functions

---
+++ tests/test_tokenization_rg.py
@@ -0,0 +1,12 @@
+from srvgd.updated_eqlearner.tokenization_rg import tokenize_eq, get_eq_string
+from check_assert import check_assert
+
+
+def test_tokenzie_eq():
+    yield check_assert, tokenize_eq('x') == [12, 1, 13]
+    yield check_assert, tokenize_eq('x0**x1') == [12, 16, 7, 17, 13]
+
+
+def test_get_eq_string():
+    yield check_assert, get_eq_string([12, 1, 13]) == 'STARTxEND'
+    yield check_assert, get_eq_string([12, 16, 7, 17, 13]) == 'STARTx0**x1END'
