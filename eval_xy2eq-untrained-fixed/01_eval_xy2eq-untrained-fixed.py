b70bf0189134dfdd4d4394ea71db13f86aff6150
Ryan Grindle <ryan.grindle@uvm.edu>
2021-04-19 12:54:50 -0400

    Added eval_xy2eq-untrained-normal/, eval_xy2eq-untrained-fixed/, eval_xy2eq-untrained-different/
    -Evaluate untrained xy2eq on normal, different, and fixed x-values

---
+++ eval_xy2eq-untrained-fixed/01_eval_xy2eq-untrained-fixed.py
@@ -0,0 +1,32 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Apr 19, 2021
+
+PURPOSE: Evaluate xy2eq-untrained-fixed and save functional form.
+
+NOTES:
+
+TODO:
+"""
+from srvgd.utils.eval import eval_nn
+
+import torch
+import numpy as np
+
+import json
+
+torch.manual_seed(1)
+np.random.seed(1)
+
+with open('00_x_list.json', 'r') as json_file:
+    x_list = np.array(json.load(json_file))
+
+with open('00_y_int_normalized_list.json', 'r') as json_file:
+    y_int_list = np.array(json.load(json_file))[:, :, 0]
+
+input_list = np.stack((x_list, y_int_list), axis=2)
+
+eval_nn(input_list=input_list,
+        model_filename=None,
+        INPUT_DIM=2)
---
+++ eval_xy2eq-untrained-normal/00_eval_xy2eq-untrained-normal.py
@@ -0,0 +1,15 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Apr 19, 2021
+
+PURPOSE: Get x and y to be used to evaluate xy2eq-untrained-normal.
+
+NOTES:
+
+TODO:
+"""
+from srvgd.utils.eval import write_x_y_lists
+
+write_x_y_lists('../datasets/equations_with_coeff_test_ff1000.csv',
+                x_type='normal')
