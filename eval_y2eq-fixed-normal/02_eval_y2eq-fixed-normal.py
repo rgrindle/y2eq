bc963890d00fc87771f16ae58e17fa14d2cb2251
Ryan Grindle <ryan.grindle@uvm.edu>
2021-04-20 20:12:15 -0400

    RMSE eval scripts now using unnormalized y

--- eval_y2eq-fixed-normal/02_eval_y2eq-fixed-normal.py
+++ eval_y2eq-fixed-normal/02_eval_y2eq-fixed-normal.py
@@ -16,14 +16,14 @@ import pandas as pd
 
 import json
 
-with open('00_y_int_normalized_list.json', 'r') as json_file:
-    y_int_normalized_list = json.load(json_file)
+with open('00_y_int_unnormalized_list.json', 'r') as json_file:
+    y_int_unnormalized_list = json.load(json_file)
 
 with open('00_y_ext_unnormalized_list.json', 'r') as json_file:
     y_ext_unnormalized_list = json.load(json_file)
 
 ff_list = pd.read_csv('01_predicted_ff.csv', header=None).values.flatten()
 
-fit_coeffs_and_get_rmse(y_int_normalized_list,
+fit_coeffs_and_get_rmse(y_int_unnormalized_list,
                         y_ext_unnormalized_list,
                         ff_list)
