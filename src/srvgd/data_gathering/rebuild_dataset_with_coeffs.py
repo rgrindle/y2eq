749cc784028bbb9e127bbd2ffa6e70263205e356
Ryan Grindle <ryan.grindle@uvm.edu>
2021-04-29 11:34:17 -0400

    Updated src/srvgd/utils/eval.py

--- src/srvgd/data_gathering/rebuild_dataset_with_coeffs.py
+++ src/srvgd/data_gathering/rebuild_dataset_with_coeffs.py
@@ -1,7 +1,7 @@
 """
 AUTHOR: Ryan Grindle
 
-LAST MODIFIED: Apr 20, 2021
+LAST MODIFIED: Apr 29, 2021
 
 PURPOSE: Make a new version of dataset_train_ff1000.pt by
          replacing the functional forms with the equations
@@ -14,7 +14,7 @@ NOTES:
 
 TODO:
 """
-from srvgd.updated_eqlearner.tokenization_rg import extract_tokens, token_map_with_coeffs
+from srvgd.updated_eqlearner.tokenization_rg import numberize_tokens, extract_tokens
 
 import torch
 from torch.utils.data import TensorDataset
@@ -30,7 +30,7 @@ eq_list = pd.read_csv(os.path.join(dataset_path, 'equations_with_coeff_train_ff1
 dataset_output = []
 for eq in eq_list:
     tokenized_eq = extract_tokens(eq)
-
+    print(eq)
     # Numbers are still grouped (e.g. 1.234 is one token).
     # Make each digit (and decimal point) a separate token.
     new_tokenized_eq = []
@@ -39,7 +39,7 @@ for eq in eq_list:
             new_tokenized_eq.extend(list(token))
         else:
             new_tokenized_eq.append(token)
-    numberized_eq = [token_map_with_coeffs[token] for token in new_tokenized_eq]
+    numberized_eq = numberize_tokens(new_tokenized_eq, two_d=False, include_coeffs=True)
     dataset_output.append(numberized_eq)
 
 # pad dataset_output
