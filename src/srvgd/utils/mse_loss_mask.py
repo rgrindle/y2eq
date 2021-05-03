23cb9624b63efb1b361a05287e2175df1556b8c7
Ryan Grindle <ryan.grindle@uvm.edu>
2021-05-03 11:15:15 -0400

    Added src/srvgd/utils/mse_loss_mask.py
    -Computes MSE but only uses indicated elements of given tensor

---
+++ src/srvgd/utils/mse_loss_mask.py
@@ -0,0 +1,33 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: May 3, 2021
+
+PURPOSE: Create a version of mse loss function
+         that will ignore certain elements. For example,
+         if the desired output is 3*x then we want the 
+         output to say coeff, *, x and we want to coeff
+         value to read 3, NaN, NaN. So, we want to calculated
+         mse on 3 but not on the others.
+
+NOTES:
+
+TODO:
+"""
+import torch
+
+
+def mse_loss_mask(predicted, target, use_index):
+    out = (predicted[:, use_index]-target[:, use_index])**2
+    return out.mean()
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+    predicted = torch.Tensor([[1, 2]])
+    target = torch.Tensor([[1, np.nan]])
+    use_index = torch.LongTensor([0])
+
+    loss = mse_loss_mask(predicted, target, use_index)
+    print('loss', loss)
