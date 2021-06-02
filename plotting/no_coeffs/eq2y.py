ed07e50176f2636845b69186b0e577b6a4d1c090
Ryan Grindle <ryan.grindle@uvm.edu>
2021-06-02 11:25:25 -0400

    Added plotting/no_coeffs/
    -compare no-coeff version to typical y2eq

---
+++ plotting/no_coeffs/eq2y.py
@@ -0,0 +1,64 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jun 1, 2021
+
+PURPOSE: Compare y2eq and y2eq-no-coeff
+
+NOTE:
+
+TODO:
+"""
+from srvgd.plotting.cdf import plot_cdf
+from srvgd.utils.eval import remove_nan
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import os
+
+model_name_list = ['eq2y-transformer',
+                   'y2eq-transformer-no-coeffs-fixed-fixed']
+rmse_filename = {'eq2y-transformer': '02_rmse.csv',
+                 'y2eq-transformer-no-coeffs-fixed-fixed': '02_rmse.csv'}
+
+
+def get_data(model_name):
+    eval_loc = os.path.join('..', '..', 'eval_'+model_name)
+
+    rmse = pd.read_csv(os.path.join(eval_loc, rmse_filename[model_name]))['rmse_int'].values
+    print(len(rmse))
+
+    return rmse
+
+
+rmse_dict = {name: get_data(name) for name in model_name_list}
+color = {0: 'C0', 1: 'C1', 2: 'C2'}
+
+
+plt.close('all')
+
+fig, axes = plt.subplots(ncols=2, sharey=True,
+                         figsize=(2*6.4, 4.8))
+
+for i, model_name in enumerate(rmse_dict):
+    for ax in axes:
+        plt.sca(ax)
+        linestyle = 'solid'
+        if i > 2:
+            linestyle = 'dashed'
+        plot_cdf(remove_nan(rmse_dict[model_name]), labels=False, linestyle=linestyle, label=model_name, color=color[i])
+        plt.xlabel('RMSE on interpolation region ($x = \\left[0.1, 0.2, \\cdots, 3.0\\right]$)')
+
+plt.sca(axes[0])
+plt.xlim([0, 3])
+plt.ylim([0, 1000])
+plt.ylabel('Cummulative counts')
+
+plt.sca(axes[1])
+plt.xscale('log')
+plt.ylim([0, 1000])
+plt.legend()
+
+plt.subplots_adjust(wspace=0.03, left=0.053, right=0.99, top=0.98)
+plt.savefig('plot_'+model_name_list[0]+'_vs_'+model_name_list[1]+'.pdf')
