3a50c6f4624e058f5dfb317dca7b75d52fe94096
Ryan Grindle <ryan.grindle@uvm.edu>
2021-06-17 09:57:04 -0400

    Updated plotting/corpus_problem/02_analyze_dataset.py
    -update based on the change of format in 01_...
    -Save plots of the smallest comparisons
    -Count how many comparisons are between different functional forms

--- plotting/corpus_problem/02_analyze_dataset.py
+++ plotting/corpus_problem/02_analyze_dataset.py
@@ -10,26 +10,48 @@ NOTES:
 
 TODO:
 """
+from srvgd.utils.rmse import RMSE
+from srvgd.updated_eqlearner.tokenization_rg import get_eq_string
+
+import torch
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-error_mat = pd.read_csv('error_mat.csv', index=False, header=None).values
-print(error_mat.shape)
+dataset = torch.load('../../datasets/dataset_train_ff1000.pt')
+Y = np.squeeze([d[0].tolist() for d in dataset])
+ff_list = [get_eq_string(d[1].tolist())[5:-3] for d in dataset]
+print(Y.shape)
 
-assert np.all(error_mat == error_mat.T), 'error_mat is not symmetric! problem in previous script'
+min_list = pd.read_csv('min_list.csv', header=None).values.flatten()
 
-# Since error_mat is symmetric and we don't really care
-# that the diagonal is all zeros, we set the upper triangular
-# part of error_mat to inf (including the diagonal). This
-# way we don't have to deal with any duplicate comparisons.
-inf_indices = np.triu_indices(50000)
-error_mat[inf_indices] = np.inf
+ind_list = pd.read_csv('ind_list.csv', header=None).values.flatten()
+assert len(ind_list) == len(min_list)
 
-sorted_index = np.argsort(error_mat)
-unrav_sorted_index = np.unravel_index(sorted_index)
-print(unrav_sorted_index[:10])
-print(error_mat[unrav_sorted_index[:10]])
+print('max error', max(min_list))
 
-pd.DataFrame(unrav_sorted_index).to_csv('unraveled_sorted_index.csv',
-                                        index=False,
-                                        header=None)
+x = np.arange(0.1, 3.1, 0.1)
+
+same_ff_count = 0
+for count, (ind, error) in enumerate(zip(ind_list, min_list)):
+    i, j = eval(ind)
+    # print(i, RMSE(Y[(i+1) % 50000], Y[j]) - error <= 10**(-20))
+    index_i = (i+1) % 50000
+    Yi = Y[index_i]
+    Yj = Y[j]
+    assert RMSE(Yi, Yj) - error <= 10**(-20)
+
+    if ff_list[index_i] == ff_list[j]:
+        same_ff_count += 1
+
+    plt.close('all')
+    plt.figure()
+    plt.plot(x, Yi, '.-', label=ff_list[index_i], alpha=0.5)
+    plt.plot(x, Yj, '.-', label=ff_list[j], alpha=0.5)
+    plt.xlabel('$x$')
+    plt.ylabel('$y$')
+    plt.title('RMSE = {:.3e}'.format(error))
+    plt.legend()
+    plt.savefig('similar_eq_in_dataset_{}.pdf'.format(count))
+
+print('same_ff_count', same_ff_count)
