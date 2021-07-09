5a22bdf2a76fd0d1a4169561aef4edcf21ce96d2
Ryan Grindle <ryan.grindle@uvm.edu>
2021-07-08 20:54:16 -0400

    Added eval_y2eq-transformer-confusing-fixed-fixed-noBFGS/

---
+++ eval_y2eq-transformer-confusing-fixed-fixed-noBFGS/00_eval_y2eq-transformer-confusing-fixed-fixed-noBFGS.py
@@ -0,0 +1,15 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jun 29, 2021
+
+PURPOSE: Get x and y to be used to evaluate y2eq-transformer-fixed-fixed.
+
+NOTES:
+
+TODO:
+"""
+from srvgd.utils.eval import write_x_y_lists
+
+write_x_y_lists('../datasets/equations_with_coeff_test_ff1000_no_coeffs.csv',
+                x_type='fixed')
---
+++ eval_y2eq-transformer-confusing-fixed-fixed-noBFGS/01_eval_y2eq-transformer-confusing-fixed-fixed-noBFGS.py
@@ -0,0 +1,20 @@
+"""
+AUTHOR: Ryan Grindle
+
+LAST MODIFIED: Jun 29, 2021
+
+PURPOSE: Evaluate y2eq-transformer-fixed-fixed and save functional form.
+
+NOTES:
+
+TODO:
+"""
+from srvgd.utils.eval import eval_y2eq_transformer
+
+import json
+
+with open('00_y_int_normalized_list.json', 'r') as json_file:
+    y_int_list = json.load(json_file)
+
+eval_y2eq_transformer(input_list=y_int_list,
+                      model_filename='BEST_y2eq_transformer_confusing_500.pt')
