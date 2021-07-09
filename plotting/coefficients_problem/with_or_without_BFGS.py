2da5df7b01357b89cabf18e4d4c2436e0c7fa02c
Ryan Grindle <ryan.grindle@uvm.edu>
2021-07-08 20:57:38 -0400

    Updated plotting/coefficients_problem/with_or_without_BFGS.py
    -Updated boxplot labels

--- plotting/coefficients_problem/with_or_without_BFGS.py
+++ plotting/coefficients_problem/with_or_without_BFGS.py
@@ -16,14 +16,14 @@ import matplotlib.pyplot as plt
 from scipy.stats import mannwhitneyu
 
 data = {}
-data['no BFGS'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-noBFGS/02_rmse.csv')['rmse_int'].values.flatten()
-data['with BFGS'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-withBFGS/02_rmse_150.csv')['rmse_int'].values.flatten()
+data['no L-BFGS-B'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-noBFGS/02_rmse.csv')['rmse_int'].values.flatten()
+data['with L-BFGS-B'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-withBFGS/02_rmse_150.csv')['rmse_int'].values.flatten()
 
 plt.boxplot(data.values(), labels=data.keys())
 plt.ylabel('Normalized RMSE on interpolation region')
 plt.yscale('log')
 plt.savefig('with_or_without_BFGS.pdf')
 
-results = mannwhitneyu(data['no BFGS'], data['with BFGS'],
+results = mannwhitneyu(data['no L-BFGS-B'], data['with L-BFGS-B'],
                        alternative='greater')
 print(results)
