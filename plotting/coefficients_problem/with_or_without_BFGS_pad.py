2e190623f82ddc61d3804b80fb3a3d61a1e8a1d0
Ryan Grindle <ryan.grindle@uvm.edu>
2021-07-08 20:58:30 -0400

    Updated plotting/coefficients_problem/with_or_without_BFGS_pad.py
    -updated boxplot labels

--- plotting/coefficients_problem/with_or_without_BFGS_pad.py
+++ plotting/coefficients_problem/with_or_without_BFGS_pad.py
@@ -16,22 +16,22 @@ import matplotlib.pyplot as plt
 from scipy.stats import mannwhitneyu
 
 data = {}
-data['y2eq no BFGS'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-noBFGS/02_rmse.csv')['rmse_int'].values.flatten()
-data['y2eq-pad no BFGS'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-noBFGS-pad/02_rmse.csv')['rmse_int'].values.flatten()
-data['y2eq-pad with BFGS'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-withBFGS-pad/02_rmse_150.csv')['rmse_int'].values.flatten()
+data['y2eq no L-BFGS-B'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-noBFGS/02_rmse.csv')['rmse_int'].values.flatten()
+data['y2eq-pad no L-BFGS-B'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-noBFGS-pad/02_rmse.csv')['rmse_int'].values.flatten()
+data['y2eq-pad with L-BFGS-B'] = pd.read_csv('../../eval_y2eq-transformer-fixed-fixed-ones-withBFGS-pad/02_rmse_150.csv')['rmse_int'].values.flatten()
 
 plt.boxplot(data.values(), labels=data.keys())
 plt.ylabel('Normalized RMSE on interpolation region')
 plt.yscale('log')
 plt.savefig('with_or_without_BFGS_pad.pdf')
 
-results = mannwhitneyu(data['y2eq-pad no BFGS'],
-                       data['y2eq-pad with BFGS'],
-                       alternative='greater')
-print('y2eq-pad no BFGS > y2eq-pad with BFGS', results)
+# results = mannwhitneyu(data['y2eq-pad no L-BFGS-B'],
+#                        data['y2eq-pad with L-BFGS-B'],
+#                        alternative='greater')
+# print('y2eq-pad no BFGS > y2eq-pad with BFGS', results)
 
 
-results = mannwhitneyu(data['y2eq no BFGS'],
-                       data['y2eq-pad no BFGS'],
+results = mannwhitneyu(data['y2eq no L-BFGS-B'],
+                       data['y2eq-pad no L-BFGS-B'],
                        alternative='two-sided')
 print('y2eq no BFGS < y2eq-pad no BFGS', results)
