46c0ba7fff489d3340b2a7c7e54ebfbc343c2e84
Ryan Grindle <ryan.grindle@uvm.edu>
2021-07-08 20:53:56 -0400

    Updated tests/test_modifying_functional_forms_list.py

--- tests/test_modifying_functional_forms_list.py
+++ tests/test_modifying_functional_forms_list.py
@@ -1,12 +1,12 @@
-from jupyter.modifying_functional_forms_list import fix_ff
+from srvgd.data_gathering.modifying_functional_forms_list import fix_ff
 from check_assert import check_assert
 
 import pandas as pd
 
 
 def test_fix_ff():
-    corrected_ff = pd.read_csv('jupyter/corrected_ff.csv', header=None).values.flatten()
-    not_corrected_ff = pd.read_csv('jupyter/not_corrected_ff.csv', header=None).values.flatten()
+    corrected_ff = pd.read_csv('corrected_ff.csv', header=None).values.flatten()
+    not_corrected_ff = pd.read_csv('not_corrected_ff.csv', header=None).values.flatten()
 
     for ff, ans in zip(not_corrected_ff, corrected_ff):
         yield check_assert, fix_ff(ff) == ans
